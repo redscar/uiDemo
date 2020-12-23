eec6225117812123a8be7aea026c50d9f2b5d4db
Brian <redscar@gmail.com>
2020-12-23 10:00:20 -0500

    Added weather icons based off description

--- scripts/app.js
+++ scripts/app.js
@@ -29,19 +29,35 @@ app.controller('weatherArea', function($scope, $http, $sce, getData) {
 
             })
         });
-    }else{
-      getData.setLocation($scope.lat, $scope.lon);
-      getData.weather().then(function mySuccess(response) {
-          $scope.weatherJson = response;
-      });
+    } else {
+        getData.setLocation($scope.lat, $scope.lon);
+        getData.weather().then(function mySuccess(response) {
+            $scope.weatherJson = response;
+        });
 
     }
 
-    $scope.getDirection= function(angle) {
-      var directions = ['North', 'North-West', 'West', 'South-West', 'South', 'South-East', 'East', 'North-East'];
-      return directions[Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8];
+    $scope.getDirection = function(angle) {
+        var directions = ['North', 'North-West', 'West', 'South-West', 'South', 'South-East', 'East', 'North-East'];
+        return directions[Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8];
     }
+    $scope.getWeatherIcon = function(weather) {
+        console.log("weather", weather);
+        console.log("Does it have cloud", weather.toLowerCase().includes("cloud"));
 
+
+        if (weather.toLowerCase().includes("cloud")) {
+            return "wi-cloud";
+        } else if (weather.toLowerCase().includes("rain")) {
+            return "wi-rain";
+        } else if (weather.toLowerCase().includes("snow")) {
+            return "wi-snow";
+        } else {
+            return "wi-day-sunny";
+
+        }
+
+    }
 
 });
 
@@ -62,10 +78,10 @@ app.service('getData', function($http) {
         return;
     }
     this.weather = function() {
-      /*
-      Get weather function. Uses openweathermap.org.
-      Documentation: https://openweathermap.org/api/one-call-api
-      */
+        /*
+        Get weather function. Uses openweathermap.org.
+        Documentation: https://openweathermap.org/api/one-call-api
+        */
         return $http.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + localData.lat + '&lon=' + localData.lon + '&appid=e996dcef9df8cd0b916530923ec04af5&units=imperial')
             .then(function mySuccess(response) {
                 return response.data;
