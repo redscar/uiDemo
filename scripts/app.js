f39504993ed5eeaac961a5198c2915461ad00b8b
Brian <redscar@gmail.com>
2020-12-23 09:44:01 -0500

    Added Wind speed/direction

--- scripts/app.js
+++ scripts/app.js
@@ -37,6 +37,11 @@ app.controller('weatherArea', function($scope, $http, $sce, getData) {
 
     }
 
+    $scope.getDirection= function(angle) {
+      var directions = ['North', 'North-West', 'West', 'South-West', 'South', 'South-East', 'East', 'North-East'];
+      return directions[Math.round(((angle %= 360) < 0 ? angle + 360 : angle) / 45) % 8];
+    }
+
 
 });
 
